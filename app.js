0ab09e2e82249b662b9912ba61c72453e6604f34
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-06-25 14:08:52 +0530

    configuring db
    -- connecting the website to the local mongoDB server
    -- ensuring the website works only with a db connection in place

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
-
+const db = require('./data/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -9,4 +9,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(authRoutes);
 
-app.listen(3000);
+
+db.connectToDatabase().then(function(){
+    app.listen(3000);
+}).catch(function(error){
+    console.log('Failed To Connect to Database');
+});
