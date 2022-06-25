da15a61dc3b7efc486942e0c22021a9991564fd9
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-06-25 19:27:27 +0530

    csurf
    -- learning and implementing csurf to provide a layer of protection
    -- error handling ejs template and middleware implementation
    -- folder movement

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const express = require('express');
-const authRoutes = require('./routes/auth.routes');
+const csrf = require('csurf');
 const db = require('./data/database');
+const authRoutes = require('./routes/auth.routes');
+const addcsrfTokenMiddleware = require('./middleware/csrf-token');
+const errorHandlerMiddleWare = require('./middleware/error-handler');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
-app.use(authRoutes);
-
+app.use(csrf());
+app.use(addcsrfTokenMiddleware);
+app.use(errorHandlerMiddleWare);
+app.use(authRoutes); 
 
 db.connectToDatabase().then(function(){
     app.listen(3000);
