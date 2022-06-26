be81a9ce2a6a260581acc432518fe763038bf452
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-06-26 12:03:07 +0530

    sessions
    -- understanding sessions csurf

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const path = require('path');
 const express = require('express');
 const csrf = require('csurf');
+const expressSession =  require('express-session');
+
 const db = require('./data/database');
 const authRoutes = require('./routes/auth.routes');
 const addcsrfTokenMiddleware = require('./middleware/csrf-token');
 const errorHandlerMiddleWare = require('./middleware/error-handler');
+const createSessionConfig = require('./config/session');
 
 const app = express();
+const sessionConfig = createSessionConfig();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:false}));
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
+
 app.use(addcsrfTokenMiddleware);
 app.use(errorHandlerMiddleWare);
+
 app.use(authRoutes); 
 
 db.connectToDatabase().then(function(){
