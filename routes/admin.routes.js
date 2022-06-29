99b7f144a5e29286b604a9d449f0a8bbbcca8f5a
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-06-29 14:00:10 +0530

    uploading images
    uploading images on website

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router = express.Router();
-
 const adminController = require('../controllers/admin.controller');
+const imageUploadMiddleware = require('../middleware/image-upload');
+
+const router = express.Router();
 
 router.get('/products', adminController.getProducts);
 router.get('/products/new', adminController.getNewProduct);
+router.post('/products',imageUploadMiddleware ,adminController.createNewProduct);
 
 module.exports = router;
